fa6ca1d4d3e2d5c6c05ff939ed566fdb6f7dc289
minjun3021 <minjun3021@naver.com>
2018-11-27 23:53:22 +0900

    navi

--- app/src/main/java/maxplus/kmj/com/maxplus/LoginActivity.java
+++ app/src/main/java/maxplus/kmj/com/maxplus/LoginActivity.java
@@ -8,11 +8,20 @@ import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
     Button reg;
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         reg=findViewById(R.id.main_b2);
+        login=findViewById(R.id.main_b1);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
         reg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
